971969b84c30079b3d67d07d13796a52869e416a
moucheh <moucheh@proton.me>
2024-01-21 21:54:06 +0100

    code cleanup, added copy and move constructor, copy and move assignment operator, initalizer_list constructor

--- 2024/my_queue_generic/Queue.hpp
+++ 2024/my_queue_generic/Queue.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include <iostream>
 #include <stdexcept>
+#include <initializer_list>
 
 template<typename T>
 class Queue {
@@ -10,32 +12,39 @@ class Queue {
 	};
 public:
 	Queue() = default;
-	Queue(Queue<T>&& drugi) : _size{drugi._size}, _top{drugi._top} {
-		drugi._top = nullptr;
-		drugi._size = 0;
+	Queue(Queue<T>&& other) : _size{other._size}, _bottom{other._bottom} {
+		other._bottom = nullptr;
+		other._size = 0;
 	}
+	Queue(const Queue<T>& other);
+	Queue<T> operator=(const Queue<T>& other);
+	Queue<T> operator=(Queue<T>&& other);
 	~Queue();
-	Queue<T>& push(const T& el);
 
+	Queue(const std::initializer_list<T>& list);
+
+	Queue<T>& push(const T& el);
 	void pop();
 
 	T& top();
 	const T& top() const;
 
-	bool empty() { return _top == nullptr; }
+	void print(std::ostream& output) const;
+
+	bool empty() { return _bottom == nullptr; }
 	unsigned long size() { return _size; }
 private:
-	Node* _top{nullptr};
 	unsigned long _size{};
+	Node* _bottom{nullptr};
 };
 
 template<typename T>
 Queue<T>& Queue<T>::push(const T& el) {
 	auto newNode = new Node{el, nullptr};
 	if (empty()) {
-		_top = newNode;
+		_bottom = newNode;
 	} else {
-		auto current = _top;
+		auto current = _bottom;
 		while (current->next != nullptr)
 			current = current->next;
 		current->next = newNode;
@@ -48,31 +57,91 @@ template<typename T>
 void Queue<T>::pop() {
 	if (empty())
 		throw std::out_of_range{"The container is empty."};
-	auto next = _top->next;
-	delete _top;
-	_top = next;
+	auto next = _bottom->next;
+	delete _bottom;
+	_bottom = next;
 	--_size;
 }
 
 template<typename T>
+Queue<T>::Queue(const Queue<T>& other) {
+	_size = other._size;
+	auto current = other._bottom;
+	while (current != nullptr) {
+		push(current->_value);
+		current = current->next;
+	}
+}
+
+template<typename T>
+Queue<T> Queue<T>::operator=(const Queue<T>& other) {
+	while (!empty()) {
+		auto temp = _bottom->next;
+		delete _bottom;
+		_bottom = temp;
+	}
+	_size = other._size;
+	auto current = other._bottom;
+	while (current != nullptr) {
+		push(current->_value);
+		current = current->next;
+	}
+}
+
+template<typename T>
+Queue<T> Queue<T>::operator=(Queue<T>&& other) {
+	while (!empty()) {
+		auto temp = _bottom->next;
+		delete _bottom;
+		_bottom = temp;
+	}
+	_size = other._size;
+	_bottom = other._bottom;
+	other._size = 0;
+	other._bottom = nullptr;
+}
+
+
+template<typename T>
 Queue<T>::~Queue() {
 	while (!empty()) {
-		auto temp = _top->next;
-		delete _top;
-		_top = temp;
+		auto temp = _bottom->next;
+		delete _bottom;
+		_bottom = temp;
 	}
+}
+
+template<typename T>
+Queue <T>::Queue(const std::initializer_list<T>& list) {
+	for (const auto& el : list)
+		push(el);
 }
 
 template<typename T>
 T& Queue<T>::top() {
 	if (empty())
 		throw std::out_of_range{"The container is empty."};
-	return _top->_value;
+	return _bottom->_value;
 }
 
 template<typename T>
 const T& Queue<T>::top() const {
 	if (empty())
 		throw std::out_of_range{"The container is empty."};
-	return _top->_value;
+	return _bottom->_value;
+}
+
+template<typename T>
+void Queue<T>::print(std::ostream& output) const {
+	auto current = _bottom;
+	while (current != nullptr) {
+		output << current->_value << ' ';
+		current = current->next;
+	}
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& output, const Queue<T>& queue) {
+	queue.print(output);
+	return output;
 }
