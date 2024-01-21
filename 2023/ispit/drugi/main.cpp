999edf83b91e78587d75e56dc861c0a4bcdcd7a9
moucheh <moucheh@proton.me>
2024-01-21 19:16:30 +0100

    Ispit iz OOP

---
+++ 2023/ispit/drugi/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using std::cout, std::endl;
+
+struct Foo {
+	Foo() {
+		cout << "DC" << endl;
+	}
+	Foo(const Foo&) {
+		cout << "CC" << endl;
+	}
+	~Foo() {
+		cout << "D" << endl;
+	}
+};
+
+void foo(Foo b) {
+	cout << "Unutar foo" << endl;
+}
+
+Foo a;
+
+int main() {
+	cout << "Pocinjemo" << endl;
+	Foo& b{a};
+	cout << "Nastavljamo" << endl;
+	foo(b);
+	cout << "Idemo dalje" << endl;
+	Foo{};
+	cout << "Zavrsavamo" << endl;
+	return 0;
+}
---
+++ 2023/ispit/prvi/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+std::vector<int> fib(int n) {
+	if (std::cin.fail())
+		throw std::runtime_error{"Morate unijeti validan broj."};
+	if (n <= 0)
+		throw std::runtime_error{"Mora biti > 0."};
+	std::vector<int> result;
+	result.push_back(1);
+	result.push_back(1);
+	for (auto i = 2; i < n; i++)
+		result.push_back(result[i - 1] + result[i - 2]);
+	return result;
+}
+
+template<typename iter, typename lambda>
+iter pronadji(iter p, iter k, lambda f) {
+	for (; p != k; ++p)
+		if (f(*p))
+			return p;
+	return k;
+}
+
+int main() {
+	int n;
+	std::cin >> n;
+	std::vector<int> sekvenca;
+	try {
+		sekvenca = fib(n);
+	} catch (const std::runtime_error& e) {
+		std::cout << e.what() << std::endl;
+		return 0;
+	}
+	auto pronadjen = pronadji(
+						 sekvenca.cbegin(),
+						 sekvenca.cend(),
+	[](int n) {
+		return n % 11 == 0;
+	});
+	if (pronadjen == sekvenca.cend())
+		std::cout << "Nije pronadjen niti jedan broj djeljiva sa 11!" << std::endl;
+	else
+		std::cout << *pronadjen << std::endl;
+	return 0;
+}
