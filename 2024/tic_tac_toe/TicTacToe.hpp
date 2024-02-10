6394b33a501488922682128ea3f56d7a28e94405
moucheh <moucheh@proton.me>
2024-02-10 18:52:41 +0100

    changed member access qualifiers

--- 2024/tic_tac_toe/TicTacToe.hpp
+++ 2024/tic_tac_toe/TicTacToe.hpp
@@ -12,9 +12,11 @@ void fix_cin();
 class TicTacToe {
 public:
 	void draw_board() const;
-
 	void play();
-
+	bool get_win_state() const { return win_state; }
+	char get_player() const { return current_player; }
+	u_int_16 get_counter() const { return counter; }
+private:
 	void change_player();
 
 	void request_player_input(u_int_16& row, u_int_16& col) const;
@@ -24,11 +26,6 @@ public:
 	bool update_board(u_int_16 row, u_int_16 col);
 
 	void check_win_state(char player);
-
-	bool get_win_state() const { return win_state; }
-	char get_player() const { return current_player; }
-	u_int_16 get_counter() const { return counter; }
-
 private:
 	matrix board{'.', '.', '.', '.', '.', '.', '.', '.', '.'};
 	char current_player{'X'};
